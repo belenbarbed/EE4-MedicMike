f4669a45826ead10049f4dc53b05fa4972b59b4c
Owen Harcombe <owen.harcombe@gmail.com>
2018-11-06 16:21:07 +0000

    Auto backup and restore working (early in the morning...)

--- Scripts/BackupDatabase.sh
+++ Scripts/BackupDatabase.sh
@@ -4,7 +4,7 @@ echo "Creating a version of the database to be pushed to git..."
 date=`date +"%b-%d-%y-%H-%M-%S"`
 mysqldump -u root -proot -x -e -A > ../Database/dbs.sql
 git checkout dev/owen
-git add dbs.sql
+git add ../Database/dbs.sql
 git commit -m "Database Backup for $date"
 git push
 git gc
