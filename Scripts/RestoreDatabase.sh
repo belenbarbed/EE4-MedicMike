addb03249eb3f876ab06704ee5ae5ad6c043191b
Owen Harcombe <owen.harcombe@gmail.com>
2018-11-06 16:18:23 +0000

    Post man pat, Post man pat...

--- Scripts/RestoreDatabase.sh
+++ Scripts/RestoreDatabase.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 cd $(dirname $0)
-read -p "This action will overwrite Baxter's database with the version stored on this branch on git. Are you sure you wish to continue?" -n 1 -r
+read -p "This action will overwrite Baxter's database with the version stored on this branch on git. Are you sure you wish to continue? (Y/y)" -n 1 -r
 echo
 password="root"
 if [[ $REPLY =~ ^[Yy]$ ]]
