da6a591ff6e04a7dad3fce4d5ede6e22aac37a45
Owen Harcombe <owen.harcombe@gmail.com>
2018-11-06 16:19:00 +0000

    and his black and white cat

--- Scripts/BackupDatabase.sh
+++ Scripts/BackupDatabase.sh
@@ -1,4 +1,5 @@
 !#/bin/sh
+cd $(dirname $0)
 echo "Creating a version of the database to be pushed to git..."
 date=`date +"%b-%d-%y-%H-%M-%S"`
 mysqldump -u root -proot -x -e -A > ../Database/dbs.sql
