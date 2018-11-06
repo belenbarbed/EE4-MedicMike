aa794e5a323d2c0308fb1b4443fa1cad24ecbffb
Owen Harcombe <owen.harcombe@gmail.com>
2018-11-06 16:10:16 +0000

    Test

--- Scripts/RestoreDatabase.sh
+++ Scripts/RestoreDatabase.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-read -p "This action will overwrite Baxter's database with the version stored on this branch on git. Are you sure you wish to continue?" -n 1 -r
+read -p "This action will overwrite Baxter's database with the version stored on this branch on git. Are you sure you wish to continue? (Y/y)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
